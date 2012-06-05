6d48a0317d89e19a748d0c0afe15b408d18dea0b
Raju Gandhi <raju.gandhi+github@gmail.com>
2012-06-05 18:19:14 -0400

    - Update noir version in project.clj - Extract out start-server to be REPL friendly

--- src/noir_addressbook/server.clj
+++ src/noir_addressbook/server.clj
@@ -3,9 +3,14 @@
 
 (server/load-views "src/noir_addressbook/views/")
 
+(defn start-server
+  ([] (start-server 8080))
+  ([port] (start-server port {:mode :dev}))
+  ([port opts] (server/start port (assoc opts :ns 'noir-addressbook))))
+
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (server/start port {:mode mode
+    (start-server port {:mode mode
                         :ns 'noir-addressbook})))
 
