9e0d6223ae1ee4a23ef9af760cede5837332373d
Raju Gandhi <raju.gandhi+github@gmail.com>
2012-06-06 21:54:27 -0400

    Heroku-fy
    - Add Procfile
    - Modify models to hook into Heroku mongo

--- src/noir_addressbook/models.clj
+++ src/noir_addressbook/models.clj
@@ -1,6 +1,23 @@
 (ns noir-addressbook.models
-  (:use somnium.congomongo))
+  (:use somnium.congomongo
+        [somnium.congomongo.config :only [*mongo-config*]]))
 
-(defn init-mongodb
-  []
-  (mongo! :db "addressbook"))
+(defn split-mongo-url [url]
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)]
+    (when (.find matcher)
+      (zipmap [:match :user :pass :host :port :db] (re-groups matcher)))))
+
+(defn init-mongodb []
+  (when (not (connection? *mongo-config*))
+    (if-let [mongo-url (get (System/getenv) "MONGOHQ_URL")]
+      (let [config (split-mongo-url mongo-url)]
+        (println "Initializing mongo @ " mongo-url)
+        (mongo! :db (:db config)
+                :host (:host config)
+                :port (Integer. (:port config)))
+        (authenticate (:user config) (:pass config))
+        (or (collection-exists? :addressbook)
+            (create-collection! :addressbook)))
+      (mongo! :db "addressbook") ; local environment
+      )))
+
